89a16987c26bfbf513df9926a186d7f9ce89a4ae
dutchTender <lzhang421@gmail.com>
2019-05-31 10:25:56 -0400

    fix for filing basis input alignment issue. #477.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/storage/properties/StorageProperties.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/storage/properties/StorageProperties.java
@@ -9,12 +9,12 @@ public class StorageProperties {
      * Folder location for storing files
      */
 
-   private String location = "/home/zhangl/images/";
+   //private String location = "/home/zhangl/images/";
 
     // load this value from property file
     //
 
-   //private String location = "C:\\images\\attorney";
+   private String location = "C:\\images\\attorney";
 
    //private String location = "/Users/stanikmas_lynn/Documents/GrantThornton/images/";
 
